9a19efd018d7b0ba926b75e394913d0c84e514d1
4rjxn <131968160+4rjxn@users.noreply.github.com>
2026-01-06 21:43:29 +0530

    :just looking at files

--- lib/Presentation/Pages/SettingsPage/settings_page.dart
+++ lib/Presentation/Pages/SettingsPage/settings_page.dart
@@ -1,3 +1,7 @@
+/*
+settings page holds all setting Options
+*/
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
